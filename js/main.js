8de781fc96aa22de4a6a36f6fc4838657be8f3f3
蓝京杭 <laamginghong@lanjingangdeAir.lan>
2017-12-23 23:34:24 +0800

    第九次提交

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var start = document.getElementById('start');
 //获取开始游戏按钮
 var start_btn = document.getElementById('start_btn');
 //获取游戏中分数显示界面
-var score = document.getElementById('score');
+var scorediv = document.getElementById('scorediv');
 //获取游戏中分数界面
 var label = document.getElementById('label');
 //获取游戏暂停界面
@@ -140,7 +140,6 @@ var zanting = function() {
 		number = 0;
 	}
 }
-
 //判断本方飞机是否飞出边界，如果飞出边界，则移除mousemove事件，反之加上mousemove事件
 var border = function() {
 	var oevent = window.event || arguments[0];
@@ -167,13 +166,13 @@ if(document.addEventListener) {
 	selfplane.imagenode.addEventListener('click', zanting, true);
 	body.addEventListener('mousemove', border, true);
 	suspend.getElementsByTagName('button')[0].addEventListener('click', zanting, true);
-	suspend.getElementsByTagName('button')[2].addEventListener('click', again, true);
+	suspend.getElementsByTagName('button')[1].addEventListener('click', again, true);
 } else if(document.attachEvent) {
 	main.attachEvent('onmousemove', move);
 	selfplane.imagenode.attachEvent('onclick', zanting);
 	body.attachEvent('onmousemove', border);
 	suspend.getElementsByTagName('button')[0].attachEvent('onclick', zanting);
-	suspend.getElementsByTagName('button')[2].attachEvent('onclick', again);
+	suspend.getElementsByTagName('button')[1].attachEvent('onclick', again);
 }
 
 //初始化隐藏本方飞机
@@ -202,13 +201,13 @@ function begin() {
 		mark1++;
 		//中飞机
 		if(mark1 % 5 == 0) {
-			enemys.push(new enemy(6, 30, 290, 46, 60, 50, 360, random(1, 3), 'img/中飞机爆炸.gif', 'img/enemy3_fly_1.png'))
+			enemys.push(new enemy(6, 30, 290, 46, 60, 50, 240, random(1, 3), 'img/中飞机爆炸.gif', 'img/enemy3_fly_1.png'))
 		}
 		//大飞机
-		if(mark1 == 20) {
-			enemys.push(new enemy(12, 60, 260, 110, 164, 100, 540, 1, 'img/大飞机爆炸.gif', 'img/enemy2_fly_1.png'));
+		if(mark1 % 20 == 0) {
+			enemys.push(new enemy(12, 60, 260, 110, 164, 100, 360, 1, 'img/大飞机爆炸.gif', 'img/enemy2_fly_1.png'));
 		} else {
-			enemys.push(new enemy(1, 10, 300, 34, 24, 10, 360, random(1, 6), 'img/小飞机爆炸.gif', 'img/enemy1_fly_1.png'));
+			enemys.push(new enemy(1, 10, 300, 34, 24, 10, 120, random(1, 6), 'img/小飞机爆炸.gif', 'img/enemy1_fly_1.png'));
 		}
 		mark = 0;
 	}
@@ -225,6 +224,18 @@ function begin() {
 			main.removeChild(enemys[i].imagenode);
 			enemys.splice(i, 1);
 			enemyslen--;
+			selfplane.planehp = selfplane.planehp - 1;
+			selfplane.imagenode.src = "img/本方飞机爆炸.gif";
+			end.style.display = 'block';
+			result.innerHTML = scores;
+			if(document.removeEventListener) {
+				main.removeEventListener('mousemove', move, true);
+				body.removeEventListener('mousemove', border, true);
+			} else if(document.detachEvent) {
+				main.detachEvent('onmousemove', move);
+				body.detachEvent('onmousemove', border);
+			}
+			clearInterval(set);
 		}
 		//当敌方飞机死亡时，经过一段时间后清除敌机
 		if(enemys[i].planeisdie == true) {
@@ -260,8 +271,8 @@ function begin() {
 		for(var j = 0; j < enemyslen; j++) {
 			//判断碰撞本方飞机
 			if(enemys[j].planeisdie == false) {
-				if(enemys[j].imagenode.offsetLeft + enemys[j].planewidth >= selfplane.imagenode.offsetLeft && enemys[j].imagenode.offsetLeft <= selfplane.imagenode.offsetLeft + selfplane.planewidth) {
-					if(enemys[j].imagenode.offsetTop + enemys[j].planeheight >= selfplane.imagenode.offsetTop + 40 && enemys[j].imagenode.offsetTop <= selfplane.imagenode.offsetTop - 20 + selfplane.planeheight) {
+				if((enemys[j].imagenode.offsetLeft + enemys[j].planewidth >= selfplane.imagenode.offsetLeft) && (enemys[j].imagenode.offsetLeft <= selfplane.imagenode.offsetLeft + selfplane.planewidth)) {
+					if((enemys[j].imagenode.offsetTop + enemys[j].planeheight >= selfplane.imagenode.offsetTop + 40) && (enemys[j].imagenode.offsetTop <= selfplane.imagenode.offsetTop - 20 + selfplane.planeheight)) {
 						//碰撞本方飞机，游戏结束，统计分数
 						selfplane.imagenode.src = "img/本方飞机爆炸.gif";
 						end.style.display = 'block';
@@ -279,20 +290,20 @@ function begin() {
 			}
 
 			//判断子弹与敌机碰撞
-			if(bullets[k].bulletimage.offsetLeft + bullets[k].bulletwidth > enemys[j].imagenode.offsetLeft && bullets[k].bulletimage.offsetLeft < enemys[j].imagenode.offsetLeft + enemys[j].planewidth) {
-				if (bullets[k].bulletimage.offsetTop<=enemys[j].imagenode.offsetTop+enemys[j].planeheight&&bullets[k].bulletimage.offsetTop+bullets[k].bulletheight>=enemys[j].imagenode.offsetTop) {
+			if((bullets[k].bulletimage.offsetLeft + bullets[k].bulletwidth > enemys[j].imagenode.offsetLeft) && (bullets[k].bulletimage.offsetLeft < enemys[j].imagenode.offsetLeft + enemys[j].planewidth)) {
+				if(bullets[k].bulletimage.offsetTop <= enemys[j].imagenode.offsetTop + enemys[j].planeheight && bullets[k].bulletimage.offsetTop + bullets[k].bulletheight >= enemys[j].imagenode.offsetTop) {
 					//敌机血量等于收到的子弹攻击力
-					enemys[j].planehp=enemys[j].planehp-bullets[k].bulletattach;
+					enemys[j].planehp = enemys[j].planehp - bullets[k].bulletattach;
 					//当敌机血量为0，低级图片还为爆炸图片，死亡标记为true,计算得分
-					if (enemys[j].planehp==0) {
-						scores=scores+enemys[j].planescore;
-						label.innerHTML=scores;
-						enemys[j].imagenode.src=enemys[j].planeboomimage;
-						enemys[j].planeisdie=true;
+					if(enemys[j].planehp == 0) {
+						scores = scores + enemys[j].planescore;
+						label.innerHTML = scores;
+						enemys[j].imagenode.src = enemys[j].planeboomimage;
+						enemys[j].planeisdie = true;
 					}
 					//删除子弹
 					main.removeChild(bullets[k].bulletimage);
-					bullets.splice(k,1);
+					bullets.splice(k, 1);
 					bulletslen--;
 					break;
 				}
